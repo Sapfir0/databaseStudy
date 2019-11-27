0bb7f7aae3daccedd0d676950ba05dfe93135194
Sapfir0 <sapfir999999@yandex.ru>
2019-11-27 14:10:14 +0400

    Разделил контроллер и модель

--- testForAndroid/DatabaseController.cs
+++ testForAndroid/DatabaseController.cs
@@ -1,68 +1,16 @@
 ﻿using System;
-using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace controlWork
-{
-    public class Citys
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public string name { get; set; }
-        public string country { get; set; }
-    }
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 
-    public class Trains
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public int number { get; set; }
-        public int companyId { get; set; }
-    }
-
-    public class Companys
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public int number { get; set; }
-    }
-
-    public class Employees
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public int companyId { get; set; }
-        public int crewId { get; set; }
-    }
-
-    public class Cruises
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public DateTime departureTime { get; set; }
-        public DateTime arrivingTime { get; set; }
-        public int trainId { get; set; }
-        public int crewId { get; set; }
-        public int trainstationSourceId { get; set; }
-        public int trainstationDestinationId { get; set; }
-    }
-
-    public class TrainstationsSource
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public string name { get; set; }
-        public int code { get; set; }
-        public int cityId { get; set; }
-    }
-
-    public class TrainstationsDestination
-    {
-        [PrimaryKey, AutoIncrement]
-        public int id { get; set; }
-        public string name { get; set; }
-        public int code { get; set; }
-        public int cityId { get; set; }
+namespace testForAndroid {
+    class DatabaseController {
     }
 }
